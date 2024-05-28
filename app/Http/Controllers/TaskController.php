e998e48e7d7d006acc06faa83a62f7525d179f72
Andres Rubio <andresrubiot5@gmail.com>
2024-05-28 08:12:47 -0500

    Tasks validate request & store, update method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -18,9 +19,10 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+        return response()->json($task, 201);
     }
 
     /**
@@ -34,9 +36,12 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TaskRequest $request, string $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->update($request->validated());
+
+        return response()->json($task);
     }
 
     /**
